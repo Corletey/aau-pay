e7313e05838997db20ae9dc2661075bc8deafff0
corletey <corleteya@gmail.com>
2024-10-16 16:53:30 +0000

    Add gitReference to use main branch

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add the gitReference property here
+  gitReference: 'main', // Changed branch to main
 })
